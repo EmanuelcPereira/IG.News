413e09b50406dea51f34fb3d252cde87dd8e5733
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-03-29 11:08:28 -0300

    Auth API implemented

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.scss';
+
+interface SubcribeButtonProps {
+  priceId: string;
+}
+export function SubscribeButton({ priceId}: SubcribeButtonProps) {
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+    >
+      Subscribe
+    </button>
+  )
+}
